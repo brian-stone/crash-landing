0e76ea70c707a12a96ad873ec59bd7f99c4a02a7
Ethan Bradley <ethadeva@gmail.com>
2020-03-08 17:38:15 -0700

    original scripts

---
+++ Whale Hunters/Assets/Scripts/BuildingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getEnergyProduction()
+    {
+        return 1;
+    }
+    public int getMetalProduction()
+    {
+        return 1;
+    }
+    public int getFoodProduction()
+    {
+        return 1;
+    }
+    public int getMaintenence()
+    {
+        return 1;
+    }
+}
