249fcfa46ae2765235c4f6dc7eb2c61f63cd358c
Daniel Liao <16liaod@berkeley.edu>
2020-03-25 16:41:37 -0700

    modifications and additions to enemyai/collider

---
+++ Whale Hunters/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Tooltip("collider to see what enemies are in detection range")]
+    public Collider2D enemyDetect;
+    [Tooltip("list of objects in det range")]
+    public List<GameObject> detectedlist;
+    /*[Tooltip("collider to see what enemies are in attack range")]
+    public Collider2D enemyinrange;
+    [Tooltip("list of objects in det range")]
+    public List<GameObject> rangelist;*/
+    //targetposition is where we move to.
+
+    private void Start()
+    {
+        detectedlist = new List<GameObject>();
+    }
+    private GameObject closestenemy(List<GameObject> detectedlist)
+    {
+        GameObject returned = gameObject;
+        double mindist = double.MaxValue;
+        foreach(GameObject enemy in detectedlist)
+        {
+            double dist = Vector2.Distance(enemy.transform.position, transform.position);
+            if (mindist > dist)
+            {
+                mindist = dist;
+                returned = enemy;
+            }
+        }
+        return returned;
+    }
+    private void Update()
+    {
+        detectedlist = enemyDetect.GetComponent<EnemyColliderScript>().getlist();
+        CombatScript.CurrentTargetPosition = closestenemy(detectedlist).transform.position;
+    }
+    
+}
---
+++ Whale Hunters/Assets/Scripts/EnemyColliderScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColliderScript : MonoBehaviour
+{
+    public List<GameObject> retlist;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "playercombatunit" || collision.gameObject.tag == "hospital" || collision.gameObject.tag == "factory" || collision.gameObject.tag == "mine" || collision.gameObject.tag == "farm" || collision.gameObject.tag == "generator")
+        {
+            retlist.Add(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (retlist.Contains(collision.gameObject))
+        {
+            retlist.Remove(collision.gameObject);
+        }
+    }
+
+    private void updatelist()
+    {
+        foreach (GameObject exists in retlist)
+        {
+            if (exists == null)
+            {
+                retlist.Remove(exists);
+            }
+        }
+    }
+
+    public List<GameObject> getlist()
+    {
+        updatelist();
+        return retlist;
+    }
+}
