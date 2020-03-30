4661d6e7e80b8470eb1ea31071efa462aa95bcff
Ethan Bradley <ethadeva@gmail.com>
2020-03-29 17:52:02 -0700

    Fixed building script, gamescript, and other changes

--- Whale Hunters/Assets/Scripts/BuildingScript.cs
+++ Whale Hunters/Assets/Scripts/BuildingScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BuildingScript : MonoBehaviour
 {
     [SerializeField] private int health;
-    [SerializeField] public GameObject[] workersinbuilding; //list of workers in each sector.
+    [SerializeField] public List<PeopleScript> workersinbuilding = new List<PeopleScript>(); //list of workers in each sector.
+    [SerializeField] public GameScript gamescript;
 
     [SerializeField] private int energy_output;
     [SerializeField] private int metal_output;
@@ -15,6 +16,8 @@ public class BuildingScript : MonoBehaviour
     [SerializeField] private int maintenance_cost;
 
     [SerializeField] private int house_capacity;
+
+    [SerializeField] private WeaponScript[] wp;//0 is sword, 1 is gun, 2 is rifle, 3 is spear
 
     // Start is called before the first frame update
     void Start()
@@ -70,7 +73,7 @@ public class BuildingScript : MonoBehaviour
 
     public void hospitalHeal()
     {
-        foreach (GameObject person in workersinbuilding)
+        foreach (PeopleScript person in workersinbuilding)
         {
             person.GetComponent<PeopleScript>().curhealth = person.GetComponent<PeopleScript>().maxhealth;
         }
@@ -78,90 +81,85 @@ public class BuildingScript : MonoBehaviour
 
     public void buildWeapon(string weapontype)
     {
-        #(I'm assuming different weapons cost different metals/energy), using 50 as placeholder for now
-        if weapontype == 'sword' and gamescript.metal >= (50) and gamescript.energy >= (50)
+        //(I'm assuming different weapons cost different metals/energy), using 50 as placeholder for now
+        if (weapontype == "sword" && gamescript.metal >= (50) && gamescript.energy >= (50))
         {
-            gamescript.weaponlist.add('sword');
+            gamescript.weapons.Add(wp[0]);
             gamescript.subtractMetal(50);
-            gamescript.subtractEnergy(50);
+            gamescript.addEnergy(-50);
         }
-        if weapontype == 'gun' and gamescript.metal >= (50) and gamescript.energy >= (50)
+        if (weapontype == "gun" && gamescript.metal >= (50) && gamescript.energy >= (50))
         {
-            gamescript.weaponlist.add('gun');
+            gamescript.weapons.Add(wp[1]);
             gamescript.subtractMetal(50);
-            gamescript.subtractEnergy(50);
+            gamescript.addEnergy(-50);
         }
-        if weapontype == 'rifle' and gamescript.metal >= (50) and gamescript.energy >= (50)
+        if (weapontype == "rifle" && gamescript.metal >= (50) && gamescript.energy >= (50))
         {
-            gamescript.weaponlist.add('rifle');
+            gamescript.weapons.Add(wp[2]);
             gamescript.subtractMetal(50);
-            gamescript.subtractEnergy(50);
+            gamescript.addEnergy(-50);
         }
-        if weapontype == 'spear' and gamescript.metal >= (50) and gamescript.energy >= (50)
+        if (weapontype == "spear" && gamescript.metal >= (50) && gamescript.energy >= (50))
         {
-            gamescript.weaponlist.add('spear');
+            gamescript.weapons.Add(wp[3]);
             gamescript.subtractMetal(50);
-            gamescript.subtractEnergy(50);
+            gamescript.addEnergy(-50);
         }
-        if weapontype != 'sword' and weapontype!='gun' and weapontype!='rifle' and weapontype != 'spear':
+        if (weapontype != "sword" && weapontype!="gun" && weapontype!="rifle" && weapontype != "spear")
         {
-            #Not sure how "print()" works here
-            gamescript.WriteLine('What are you trying to build?');
-        }
-        else:
-        {
-            gamescript.WriteLine("Not enough resources");
-        }
-    }
-    
-    #Alternative, delete if unused:
-    #public void buildWeapon(int i, int f) #i=gamescript.metal, f=gamescript.energy <- probably unneeded
-    #{
-        #(4 weapons, sword, gun, rifle, spear)
-        #gamescript checks if resources are enough??? -> so I guess gamescript checks resources and if resources are enough run buildWeapon(), not sure how different kinds of weapons are made though...
-        #gamescript.weaponlist.add('name');
-        #^I'm guessing "name" is supposed to be one of the 4 weapon types
-        #gamescript.subtractMetal(50);
-        #gamescript.subtractEnergy(50);
-    #}
-    
-    
-    
-    public void tagcheck():
-    {
-        #what is this supposed to return? Are we clicking on a building to get the building's name or something?
-        gameobject.getTag();
-    }
-    
-    
-    public void populate(gameobject[])
-    {
-        if house_capacity > 0 #Alternatively "if len(workersinbuilding)<house_capacity", but that will change some code below.
-        {
-            workersinbuilding.add(gameobject[]); #not too sure how this works...
-            house_capacity-=1;
-            #I'm assuming house_capacity is a number > 0 that shows how many people can fit into house
-            #is there a list of people (similar to workersinbuilding) of people not in buildings? Form my understanding people not in buildings do work.
-            #workersnotinbuilding.remove(gameobject[])
-        }
-        if house_capacity<=0 #else
-        {
-            gamescript.WriteLine('Building is full, find somewhere else.');
-        }
-    }
-    
-    public void depopulate(gameobject[])
-    {
-        if len(workersinbuilding) == 0      #redundant???
-        {
-            gamescript.WriteLine("No one's here...");
+            //Not sure how "print()" works here
+            //gamescript.WriteLine('What are you trying to build?');
         }
         else
         {
-            workersinbuilding.remove(gameobject[]);
-            #workersnotinbuilding.add(gameobject[]);
-            house_capacity+=1;
+            //gamescript.WriteLine("Not enough resources");
         }
+    }
+    
+    //Alternative, delete if unused:
+    /*public void buildWeapon(int i, int f) #i=gamescript.metal, f=gamescript.energy <- probably unneeded
+    #{
+        #(4 weapons, sword, gun, rifle, spear)
+        #gamescript checks if resources are enough??? -> so I guess gamescript checks resources and if resources are enough run buildWeapon(), not sure how different kinds of weapons are made though...
+        #gamescript.weapons.add('name');
+        #^I'm guessing "name" is supposed to be one of the 4 weapon types
+        #gamescript.subtractMetal(50);
+        #gamescript.subtractEnergy(50);
+    #}*/
+    
+    
+    
+    public string tagcheck()
+    {
+        //what is this supposed to return? Are we clicking on a building to get the building's name or something?
+        return this.gameObject.tag;
+    }
+    
+    
+    public void populate(PeopleScript a)
+    {
+        if (house_capacity > 0) //Alternatively "if len(workersinbuilding)<house_capacity", but that will change some code below.
+        {
+            workersinbuilding.Add(a); //not too sure how this works...
+            house_capacity-=1;
+            //I'm assuming house_capacity is a number > 0 that shows how many people can fit into house
+            //is there a list of people (similar to workersinbuilding) of people not in buildings? Form my understanding people not in buildings do work.
+            //workersnotinbuilding.remove(gameobject[])
+        }
+        if (house_capacity<=0)
+        {
+            //gamescript.WriteLine('Building is full, find somewhere else.');
+        }
+    }
+    
+    public void depopulate(PeopleScript p)
+    {
+        
+        workersinbuilding.Remove(p);
+        //workersnotinbuilding.add(gameobject[]);
+        house_capacity+=1;
+        
     }
     
 }
